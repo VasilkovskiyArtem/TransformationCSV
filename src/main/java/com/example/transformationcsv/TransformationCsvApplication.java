2daa361d216d92f89a5b0213f9bf12ed9a6762f6
Vasilkovskiy Artem <va.work.dev@gmail.com>
2023-02-21 16:08:28 +0800

    Initial commit

---
+++ src/main/java/com/example/transformationcsv/TransformationCsvApplication.java
@@ -0,0 +1,13 @@
+package com.example.transformationcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformationCsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransformationCsvApplication.class, args);
+    }
+
+}
